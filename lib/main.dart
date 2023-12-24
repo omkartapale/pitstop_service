f20f113585e159dfc2f18d8b773ed8f65506be69
Omkar Tapale <omkartapale@gmail.com>
2023-12-24 15:33:21 +0530

    feat(UI): Bundling google fonts
    - Add fonts to asset
    - Add font licensing to app license registry

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pitstop_service/app.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(const MainApp());
 }
 
